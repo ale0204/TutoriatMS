695a882b968b67f06935a1a069dbee439eb15aef
ale0204 <nalexandra291@gmail.com>
2025-04-13 19:01:01 +0300

    Etapele 1-3 - typo fix

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Serverrr running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
